5c519566abd21398736da5daa96295d3b0ca35bf
Mohamed Fares <fares4697@gmail.com>
2025-12-06 16:37:35 +0200

    courses migrations

---
+++ migrations/20251206143447-courses-table.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("courses", {
+      course_id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addIndex("courses", ["title"]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("courses");
+  },
+};
