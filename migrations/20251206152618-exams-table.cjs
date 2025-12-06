d8c46503eb0aee5cdac64a728e1e6b652f80dcf3
Mohamed Fares <fares4697@gmail.com>
2025-12-06 17:32:41 +0200

    exams migration

--- migrations/20251206152618-exams-table.cjs
+++ migrations/20251206152618-exams-table.cjs
@@ -3,23 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("professors", {
-      professorId: {
+    await queryInterface.createTable("exams", {
+      examId: {
         type: Sequelize.UUID,
         primaryKey: true,
         unique: true,
         allowNull: false,
         defaultValue: Sequelize.UUIDV4,
-      },
-      userId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        references: {
-          model: "users",
-          key: "user_id",
-        },
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE",
       },
       courseId: {
         type: Sequelize.UUID,
@@ -30,10 +20,6 @@ module.exports = {
         },
         onDelete: "CASCADE",
         onUpdate: "CASCADE",
-      },
-      title: {
-        type: Sequelize.ENUM("professor", "teaching_assistant"),
-        allowNull: false,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -49,10 +35,9 @@ module.exports = {
         type: Sequelize.DATE,
       },
     });
-    await queryInterface.addIndex("professors", ["title"]);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("professors");
+    await queryInterface.dropTable("exams");
   },
 };
